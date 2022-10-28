038c4f574a8f93b047b95e1cf6481693b58d3728
MeushDashaV <meyshuha@gmail.com>
2022-10-28 17:47:46 +0300

    js

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 (function getTimer() {
-    /*const countdown = document.querySelector('.time');
+    const countdown = document.querySelector('.time');
     const days = document.getElementById('days');
     const hours = document.getElementById('hours');
     const minutes = document.getElementById('minutes');
@@ -9,7 +9,7 @@
     const countdownTime = new Date().getFullTime();
     const dealOfTheMonth = new Date(`Desember 01 ${countdownTime + 1} 00:00:00`);
     month.innerText = countdownTime + 1;
-    //Update countdown time
+
     function updateCountdown() {
         const currentTime = new Date();
         const diff = dealOfTheMonth - currentTime;
@@ -18,25 +18,20 @@
         const h = Math.floor(diff / 1000 / 60 / 60) % 24;
         const m = Math.floor(diff / 1000 / 60) % 60;
         const s = Math.floor(diff / 1000) % 60;
-        //DOM
+
         days.innerHTML = d;
         hours.innerHTML = h < 10 ? '0' + h : h;
         minutes.innerHTML = m < 10 ? '0' + m : m;
         seconds.innerHTML = s < 10 ? '0' + s : s;
     }
 
-    //spinner
+
     setTimeout(() => {
         loading.remove();
         countdown.style.display = 'flex';
     }, 1000);
-    // Run every second
-    setInterval(updateCountdown, 1000);*/
 
-   let date_new = "December 21,2022 18:00:00";
-    
-    let date_t = new Date(date_new);
-    let date = new Date();
-let timer = date_t - date;
+    setInterval(updateCountdown, 1000);
+
 
 });
