6bcdebeab0f22cf12a75d3bb6c48821593dbedc7
MeushDashaV <meyshuha@gmail.com>
2022-10-28 18:40:23 +0300

    js

--- js/countdown.js
+++ js/countdown.js
@@ -1,37 +1,29 @@
-(function getTimer() {
-    const countdown = document.querySelector('.time');
-    const days = document.getElementById('days');
-    const hours = document.getElementById('hours');
-    const minutes = document.getElementById('minutes');
-    const seconds = document.getElementById('seconds');
-    const loading = document.getElementById('loading');
+(function () {
 
-    const countdownTime = new Date().getFullTime();
-    const dealOfTheMonth = new Date(`Desember 01 ${countdownTime + 1} 00:00:00`);
-    month.innerText = countdownTime + 1;
+    const days = document.querySelector('.days');
+    const hours = document.querySelector('.hours');
+    const minutes = document.querySelector('.minutes');
+    const seconds = document.querySelector('.seconds');
 
-    function updateCountdown() {
+    function countDown () {
+        const deadLineDate = new Date(`Desember 01 2022 00:00:00`);
         const currentTime = new Date();
-        const diff = dealOfTheMonth - currentTime;
+        const diff = deadLineDate - currentTime;
 
-        const d = Math.floor(diff / 1000 / 60 / 60 / 24);
-        const h = Math.floor(diff / 1000 / 60 / 60) % 24;
-        const m = Math.floor(diff / 1000 / 60) % 60;
-        const s = Math.floor(diff / 1000) % 60;
+        const days = Math.floor(diff / 1000 / 60 / 60 / 24);
+        const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
+        const minutes = Math.floor(diff / 1000 / 60) % 60;
+        const seconds = Math.floor(diff / 1000) % 60;
 
-        days.innerHTML = d;
-        hours.innerHTML = h < 10 ? '0' + h : h;
-        minutes.innerHTML = m < 10 ? '0' + m : m;
-        seconds.innerHTML = s < 10 ? '0' + s : s;
-    }
+        days.innerText = deys < 10 ? '0' + deys : deys;
+        hours.innerText = hours < 10 ? '0' + hours : hours;
+        minutes.innerText = minutes < 10 ? '0' + minutes : minutes;
+        seconds.innerText = seconds < 10 ? '0' + seconds : seconds;
+    };
 
-
-    setTimeout(() => {
-        loading.remove();
-        countdown.style.display = 'flex';
-    }, 1000);
-
-    setInterval(updateCountdown, 1000);
+   
+    countDown();
+    setInterval(countDown, 1000);
 
 
-});
+})();
