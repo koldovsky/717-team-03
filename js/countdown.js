5cd68a912d9ff9136db0af480408264af0d56324
MeushDashaV <meyshuha@gmail.com>
2022-10-29 19:44:45 +0300

    js

--- js/countdown.js
+++ js/countdown.js
@@ -1,16 +1,16 @@
 (function () {
 
     const countdownContainer = document.querySelector('.countdown');
-    const days = document.querySelector('.days');
-    const hours = document.querySelector('.hours');
-    const minutes = document.querySelector('.minutes');
-    const seconds = document.querySelector('.seconds');
+    const days = document.getElementById('days');
+    const hours = document.getElementById('hours');
+    const minutes = document.getElementById('minutes');
+    const seconds = document.getElementById('seconds');
 
-
+ 
 
 
     function countdown () {
-        const deadLineDate = new Date(`December 01 2022 00:00:00`);
+        const deadLineDate = new Date(`November 01 2022 00:00:00`);
         const currentTime = new Date();
         const diff = deadLineDate - currentTime;
 
@@ -19,10 +19,10 @@
         const m = Math.floor(diff / 1000 / 60) % 60;
         const s = Math.floor(diff / 1000) % 60;
 
-        days.innerText = d < 10 ? '0' + d : d;
-        hours.innerText = h < 10 ? '0' + h : h;
-        minutes.innerText = m < 10 ? '0' + m : m;
-        seconds.innerText = s < 10 ? '0' + s : s;
+        days.innerHTML = d < 10 ? '0' + d : d;
+        hours.innerHTML = h < 10 ? '0' + h : h;
+        minutes.innerHTML = m < 10 ? '0' + m : m;
+        seconds.innerHTML = s < 10 ? '0' + s : s;
     };
 
     countdown();
